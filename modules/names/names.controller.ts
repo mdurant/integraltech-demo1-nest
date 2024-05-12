b7a74d38946cb9462fa0f842e08acd47ab134b45
Mauricio Durán <mauriciodurant@gmail.com>
2024-05-12 00:02:17 -0400

    add method getNames

--- modules/names/names.controller.ts
+++ modules/names/names.controller.ts
@@ -1,7 +1,9 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { NamesService } from './names.service';
+import { ApiBody, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/v1/names')
+@ApiTags('Api Names')
 export class NamesController {
 
     constructor( private namesService: NamesService) {
@@ -9,7 +11,33 @@ export class NamesController {
     }
 
     @Post()
+    @ApiBody({ description: 'Añadir un nombre', examples: { 'Ej.1': { value: { name: 'Jose'}}, 'Ej.2': { value: { name: 'Maria'}}} })
+    @ApiOperation({description: 'Create a new name'})
     createName(@Body() data: {name: string}){
         return this.namesService.createName(data.name);
+    }
+
+    @Get()
+    @ApiOperation({description: 'Get all names'})
+    @ApiQuery({name: 'filter', type: 'string', required: false, description: 'Filter by name'})
+    getNames(@Query() query:any){
+        console.log('query', query);
+        return this.namesService.getNames();
+    }
+
+    @Put('/:name/:newName')
+    @ApiParam({name: 'name', type: 'string', description: 'Name to update'})
+    updateName(@Param('name') name: string, @Param('newName') newName: string){
+        return this.namesService.updateName(name, newName);
+    }
+
+    @Delete('clear')
+    clearNames(){
+        return this.namesService.clearNames();
+    }
+
+    @Delete('/:name')
+    deleteName(@Param('name') name: string){
+        return this.namesService.deleteName(name);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Título de tu API')
+    .setDescription('Descripción de tu API')
+    .setVersion('1.0')
+    .addTag('tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
